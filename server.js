1a8a3b108ef7b5d90f06445f5f8a9b946f778581
Minh Nguyen <ngmikeng@gmail.com>
2018-07-16 23:58:51 +0700

    Add eslint.

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./config/app');
 
 // Server is listenning on port {config.port}
 app.listen(config.port, () => {
-  console.info(`Server started on port ${config.port} (${config.env})`);
+  console.info(`Server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
 });
